9e7bcf5082d58ce6046204e661866ec70df327f8
unknown <823903327@qq.com>
2018-12-09 15:26:30 +0800

    项目初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    我是首页
+    <ul>
+      <li @longtap="delOne(index)" v-for="(item,index) in list">
+        {{item.name}}--{{item.skill}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          name: "lmy",
+          skill: "唱歌"
+        },
+        {
+          name: "jzf",
+          skill: "跳舞"
+        },
+        {
+          name: "lgvd",
+          skill: "玩"
+        }
+      ]
+    };
+  },
+  methods: {
+    delOne(index) {
+      wx.showModal({
+        title: "提示",
+        content: "你确定删除吗？"
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
